4fbbc4c901407af0b4deecdb6ef41b886641f87a
Delphin <delphinrukundo10@gmail.com>
2023-11-25 18:51:29 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { data, error } = await useFetch(`/api/pikacjdkdshu`);
+
+</script>
+
+<template>
+    <h1>Home page for Nuxt 3</h1>
+    <div>
+     <ul v-if="data">
+        <li>
+            <img :src="data.sprite" :alt="data.name" />
+        </li>
+        <li>
+            <h2>{{ data.name }}</h2>
+        </li>
+     </ul>
+     <div v-else>
+        {{ error }}
+     </div>
+    </div>
+</template>
